fc23c3499b870bc526f389e518ef8e6a13745752
Lukas <MuhammedLukman2001@gmail.com>
2025-03-15 22:04:10 +0100

    Grid updated

--- src/features/homepage/GridUi.jsx
+++ src/features/homepage/GridUi.jsx
@@ -1,11 +1,3 @@
-// echo "# Positivus" >> README.md
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/DevLukman/Positivus.git
-// git push -u origin main
-
 import GridUiLg from "./GridUiLg";
 import GridUimobile from "./GridUimobile";
 
