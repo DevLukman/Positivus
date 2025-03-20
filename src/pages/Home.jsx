a707451dbf707eae148b9bf18daaa33a312e2c08
Lukas <MuhammedLukman2001@gmail.com>
2025-03-20 15:30:42 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,12 @@ import Marquee from "../features/homepage/Marquee";
 import Team from "../features/homepage/Team";
 import Testimonial from "../features/homepage/Testimonial";
 import Navbar from "../ui/Navbar";
-import Preloader from "../ui/Preloader";
 import Contact from "../features/homepage/Contact";
 import Footer from "../ui/Footer";
 import MakeThingsHappen from "../features/homepage/MakeThingsHappen";
 function Home() {
   return (
     <>
-      <Preloader />
       <Navbar />
       <HeroSection />
       <Marquee />
